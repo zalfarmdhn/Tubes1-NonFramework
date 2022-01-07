4d1fcf5331b23f7055f7b9d23f583e6cb06ba009
zalfarmdhn <zalfarmdhn1011@gmail.com>
2022-01-07 20:46:15 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const mainMenu = document.querySelector('.navbar-right');
+const closeMenu = document.querySelector('.closemenu');
+const openMenu = document.querySelector('.openmenu');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+      // sticky navbar on scroll script
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }else{
+          $('.navbar').removeClass("sticky");
+      }
+  });
+});
+
+
